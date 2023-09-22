a67bc97fdeb4c6ccf46c85a8e37f191a39653e54
Rayhan Fallah <rayhanfallah29@gmail.com>
2023-09-22 23:42:55 +0700

    Not responsive yet

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ $("form").on("submit", (e) => {
   validateEmail($("form input").val());
 });
 
+$(".success button").click((e) => {
+  e.preventDefault();
+  $(".container").removeClass("d-none");
+  $(".success").addClass("d-none");
+});
+
 function validateEmail(input) {
   var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if ($("form input").val().match(validRegex)) {
     $("form p").addClass("d-none");
     $(".container").addClass("d-none");
     $(".success").removeClass("d-none");
-    return true;
+    $("form input").removeClass("invalid");
   } else {
     $("form p").removeClass("d-none");
+    $("form input").addClass("invalid");
     return false;
   }
 }
